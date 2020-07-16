a194e8c0241dc313bca7b722d84830f994efdf10
Ed Sweeney <ed@onextent.com>
2020-07-16 16:49:08 -0700

    apiOrigin

--- CONFIG_FROM_ENV.sh
+++ CONFIG_FROM_ENV.sh
@@ -1,6 +1,7 @@
 sed "s/##CLIENT_ID##/${CLIENT_ID}/g" "./src/auth_config_example.json" |
 sed "s/##DOMAIN##/${DOMAIN}/g" |
 sed "s~##APP_ORIGIN##~${APP_ORIGIN}~g" |
+sed "s~##API_ORIGIN##~${API_ORIGIN}~g" |
 sed "s~##API_IDENTIFIER##~${API_IDENTIFIER}~g" > "./src/auth_config.json"
 
 echo "configured for domain $DOMAIN and origin ${APP_ORIGIN}"
