aebc0a80832c483703fc7c6ede5581dad4e5e67a
Ed Sweeney <ed@onextent.com>
2020-07-19 17:37:22 -0700

    new api demo path

--- api-server.js
+++ api-server.js
@@ -35,7 +35,7 @@ const checkJwt = jwt({
   algorithm: ["RS256"]
 });
 
-app.get("/api/external", checkJwt, (req, res) => {
+app.get("/dtlab-alligator/type/machinery", checkJwt, (req, res) => {
   res.send({
     msg: "Your access token was successfully validated!"
   });
